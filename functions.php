789a5d0cf74e7ac731996916dabb015967e72f93
blnche <peltierblanche@gmail.com>
2023-09-12 11:00:40 +0200

    script js added and working

--- functions.php
+++ functions.php
@@ -138,7 +138,11 @@ add_action('customize_register', 'theme_customizer_register');*/
 function getNavigationMenu()
 {
     $items = wp_get_nav_menu_items('Menu de la page d\'accueil');
-
     return $items;
 }
+
+function add_theme_scripts() {
+    wp_enqueue_script('main', get_template_directory_uri().'/assets/js/main.js');
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
 ?>
