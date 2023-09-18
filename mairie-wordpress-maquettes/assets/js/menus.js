52682cab231e3370f0bc633b7aa2fb5fee0215bf
blnche <peltierblanche@gmail.com>
2023-09-18 12:39:00 +0200

    worked on changing icons colors for light and dark theme

--- mairie-wordpress-maquettes/assets/js/menus.js
+++ mairie-wordpress-maquettes/assets/js/menus.js
@@ -5,10 +5,10 @@ export function burgerMenu () {
     burgerMenuButton.addEventListener('click', function () {
         menu.classList.toggle('active');
         if (menu.getAttribute('class') === 'active') {
-            burgerMenuButton.setAttribute('src', 'https://img.icons8.com/sf-regular-filled/48/multiply.png');
+            burgerMenuButton.setAttribute('src', 'https://img.icons8.com/FFFFFF/sf-regular-filled/48/multiply.png');
             burgerMenuButton.setAttribute('alt', 'multiply');
         } else {
-            burgerMenuButton.setAttribute('src', 'https://img.icons8.com/fluency-systems-filled/48/menu.png');
+            burgerMenuButton.setAttribute('src', 'https://img.icons8.com/FFFFFF/fluency-systems-filled/48/menu.png');
             burgerMenuButton.setAttribute('alt','menu');
         }
     })
